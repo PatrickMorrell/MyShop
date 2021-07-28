0df3df43df2a2dacd1d562114b2b14129d726b7d
PatrickMorrell <43747743+PatrickMorrell@users.noreply.github.com>
2021-07-28 08:35:01 +0100

    all files

--- js/mybasket.js
+++ js/mybasket.js
@@ -2,7 +2,6 @@ var basket = [];
 if (localStorage.getItem('basket')) {
 	basket = localStorage.getItem('basket').split(',').map(Number);
 }
-// console.log(basket)
 
 function displayBasket() {
 	var quantities = [];
@@ -24,21 +23,22 @@ function displayBasket() {
 	var total = 0;
 	quantities.forEach(function(item) {
 		var pdt;
-		//console.log(item.id);
 		pdt = products.find(p => p.id == item.id);
 		$('#tblBasket').append('<tr>')
-		$('#tblBasket').append('<td>' + pdt.name + '</td>')
-		$('#tblBasket').append('<td>&#163;' + pdt.price + '</td>')	
-		$('#tblBasket').append('<td>' + item.amount + '</td>')
-		$('#tblBasket').append('<td><button id="btnAdd' + pdt.id + '" onclick="removeFromBasket(' + pdt.id + ')">Remove Item</button></td>')
-		$('#tblBasket').append('/<tr>')
+		.append('<td>' + pdt.name + '</td>')
+		.append('<td>&#163;' + pdt.price + '</td>')	
+		.append('<td>' + item.amount + '</td>')
+		.append('<td><button id="btnAdd' + pdt.id + '" onclick="removeFromBasket(' + pdt.id + ')">Remove Item</button></td>')
+		.append('/<tr>')
 		total += (pdt.price * item.amount)
 	})
 
 	$('#tblBasket').append('<tr>')
-	$('#tblBasket').append('<td>Total</td>')
-	$('#tblBasket').append('<td>&#163;' + total + '</td>')
-	$('#tblBasket').append('</tr>')
+	.append('<td>Total</td>')
+	.append('<td>&#163;' + total + '</td>')
+	.append('</tr>')
+	.append('<tr><td><a href="checkout.html">Checkout</a></td></tr>')
+	localStorage.setItem('total', total)
 }
 
 
--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ function displayProducts() {
 	$('#tblProducts').append('<tr><td>Product</td><td>Cost</td></tr>');
 	products.forEach( function(pdt) {
 		$('#tblProducts').append('<tr>')
-		$('#tblProducts').append('<td>' + pdt.name + '</td>')
-		$('#tblProducts').append('<td>&#163;' + pdt.price + '</td>')
-		$('#tblProducts').append('<td><button id="btnAdd' + pdt.id + '" onclick="addToBasket(' + pdt.id + ')">Add Item</button></td>')
-		$('#tblProducts').append('/<tr>')
+		.append('<td>' + pdt.name + '</td>')
+		.append('<td>&#163;' + pdt.price + '</td>')
+		.append('<td><button id="btnAdd' + pdt.id + '" onclick="addToBasket(' + pdt.id + ')">Add Item</button></td>')
+		.append('/<tr>')
 	})
 }
 
